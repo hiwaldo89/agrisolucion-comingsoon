80060725fb3e999439ee4f9cdbc00fd80bd7cd63
Oswaldo Zárate <oswaldo@somosbloom.com>
2020-01-30 21:05:47 -0600

    add mailer.php

--- mailer.php
+++ mailer.php
@@ -9,37 +9,50 @@ require 'vendor/autoload.php';
 
 $mail = new PHPMailer(true);
 
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp1.example.com';                    // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'user@example.com';                     // SMTP username
-    $mail->Password   = 'secret';                               // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-    $mail->Port       = 587;                                    // TCP port to connect to
+$name = $_POST["name"];
+$emailAddress = $_POST["email"];
+$message = $_POST["message"];
+$myEmail = $_POST["myemail"];
 
-    //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-    $mail->addAddress('ellen@example.com');               // Name is optional
-    $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
+if ( trim($myEmail) !== "" || trim($name) === "" || trim($message) === "" || trim($emailAddress) === "" ) {
+    echo "error";
+} else {
+    try {
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+        $mail->isSMTP();                                            // Send using SMTP
+        $mail->Host       = 'smtp.zoho.com';                    // Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+        $mail->Username   = 'contacto@agrisolucion.com';                     // SMTP username
+        $mail->Password   = 'Hid.algo88';                               // SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+        $mail->Port       = 587;                                    // TCP port to connect to
+    
+        //Recipients
+        $mail->setFrom('contacto@agrisolucion.com', 'Agrisolución');
+        $mail->addAddress('contacto@agrisolucion.com', 'Agrisolución');     // Add a recipient
+        //$mail->addAddress('ellen@example.com');               // Name is optional
+        //$mail->addReplyTo('info@example.com', 'Information');
+        //$mail->addCC('cc@example.com');
+        //$mail->addBCC('bcc@example.com');
+    
+        // Attachments
+        //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+        //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+    
+        // Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'Mensaje desde el sitio web';
+        $mail->Body    = 'Nombre: ' . $name . '<br>' .
+                         'Correo: ' . $emailAddress . '<br>' .
+                         'Mensaje: ' . $message;
+        $mail->AltBody = 'Nombre: ' . $name . '; Email: ' . $emailAddress . '; Mensaje: ' . $message;
 
-    // Attachments
-    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-
-    // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        $mail->CharSet = 'UTF-8';
+    
+        $mail->send();
+        return 'success';
+    } catch (Exception $e) {
+        echo "error";
+    }
 }
--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import $ from "jquery";
 import "slick-carousel";
+import axios from "axios";
 
 $(".home-slider").slick({
   arrows: false,
   dots: true,
-  fade: true
+  fade: true,
+  autoplay: true
 });
 
 const hamburger = document.querySelector(".hamburger");
@@ -14,4 +16,34 @@ hamburger.onclick = e => {
   e.preventDefault();
   hamburger.classList.toggle("is-active");
   navBar.classList.toggle("is-visible");
+};
+
+const contactForm = document.querySelector("form");
+const loadingIcon = document.querySelector(".lds-ring");
+const formMessage = document.querySelector(".form-message");
+contactForm.onsubmit = e => {
+  e.preventDefault();
+  loadingIcon.classList.remove("loading");
+  loadingIcon.classList.add("loading");
+  formMessage.innerHTML = "";
+
+  axios({
+    method: "post",
+    url: "/mailer.php",
+    data: new FormData(contactForm)
+  })
+    .then(res => {
+      if (res.data !== "error") {
+        formMessage.innerHTML =
+          "Hemos recibido tu mensaje, te contactaremos en breve.";
+      } else {
+        formMessage.innerHTML = "Ocurrió un error, intenta de nuevo.";
+      }
+    })
+    .catch(e => {
+      formMessage.innerHTML = "Ocurrió un error, intenta de nuevo.";
+    })
+    .finally(() => {
+      loadingIcon.classList.remove("loading");
+    });
 };
